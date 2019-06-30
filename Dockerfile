efa4c1fb5fa8c6b3b3d9dd43b941ae7a71cdcd9f
Adam Xiao <iefcuxy@gmail.com>
2019-06-30 16:04:39 +0800

    centos 7.4 squid add https ssl-bump and url_rewrite

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,21 @@
 FROM centos:7.4.1708
 LABEL maintainer="iefcuxy@gmail.com"
 
+ENV DEBIAN_FRONTEND noninteractive
+
 ENV SQUID_VERSION=3.5.27 \
     SQUID_CACHE_DIR=/var/spool/squid \
     SQUID_LOG_DIR=/var/log/squid \
-    SQUID_USER=proxy
+    SQUID_USER=squid
 
 RUN yum makecache \
  && yum install -y squid ruby
 
+ADD ./files /files
 COPY entrypoint.sh /sbin/entrypoint.sh
-RUN chmod 755 /sbin/entrypoint.sh
+RUN mv /files/squid/* /etc/squid/ \
+    && chmod +x /etc/squid/url_rewriter.rb \
+    && chmod 755 /sbin/entrypoint.sh
 
 EXPOSE 3128/tcp
 ENTRYPOINT ["/sbin/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -13,25 +13,26 @@ create_cache_dir() {
 }
 
 create_log_dir
-create_cache_dir
+#create_cache_dir
 
 # allow arguments to be passed to squid
 if [[ ${1:0:1} = '-' ]]; then
   EXTRA_ARGS="$@"
   set --
-elif [[ ${1} == squid || ${1} == $(which squid) ]]; then
+elif [[ ${1} == squid || ${1} == /usr/sbin/squid ]]; then
   EXTRA_ARGS="${@:2}"
   set --
 fi
 
 # default behaviour is to launch squid
 if [[ -z ${1} ]]; then
-  if [[ ! -d ${SQUID_CACHE_DIR}/00 ]]; then
-    echo "Initializing cache..."
-    $(which squid) -N -f /etc/squid/squid.conf -z
-  fi
+  /usr/lib64/squid/ssl_crtd -c -s /var/lib/ssl_db -M 4MB
+  #if [[ ! -d ${SQUID_CACHE_DIR}/00 ]]; then
+    #echo "Initializing cache..."
+    /usr/sbin/squid -N -f /etc/squid/squid.conf -z
+  #fi
   echo "Starting squid..."
-  exec $(which squid) -f /etc/squid/squid.conf -NYCd 1 ${EXTRA_ARGS}
+  exec /usr/sbin/squid -f /etc/squid/squid.conf -NYCd 1 ${EXTRA_ARGS}
 else
   exec "$@"
 fi
